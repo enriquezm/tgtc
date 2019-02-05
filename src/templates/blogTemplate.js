df54896702c2a87d16d5481851888c0641ab83e1
Myles Enriquez <enri.myles@gmail.com>
2019-02-04 21:35:45 -0800

    Set up post template.

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,33 +1,80 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from 'react';
+import { graphql, Link } from 'gatsby';
+import '../global-styles.css';
+import styled from 'styled-components';
 
-export default function Template({ data }) { // 'data' is injected by the Graphql query below
-  const { markdownRemark } = data // data.markdownRemark holds our post data
-  const { frontmatter, html } = markdownRemark
+const PostContentContainer = styled.div`
+  h1, h2, h3, h4, h5, h6 {
+    margin-bottom: 10px;
+  }
+  p {
+    margin-bottom: 20px;
+  }
+`;
+  
+  
+const Template = ({data, pageContext}) => { // 'data' is injected by the Graphql query below
+  
+  const {prev, next} = pageContext; 
 
+  const {markdownRemark} = data; // data.markdownRemark holds our post data
+  const title = markdownRemark.frontmatter.title;
+  const html = markdownRemark.html;
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{ frontmatter.title }</h1>
-        <h2>{ frontmatter.date }</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
+    <div>
+      <header className="flex-container flex-split margin-bottom-lg">
+        <div>
+          <div className="flex-content">
+            <Link to="/"><h1>Myles Enriquez</h1></Link>
+            <p className="font-color-violet">Frontend Engineer</p>
+            <div>
+              <a className="link-btn" href="#">resume</a>
+              <a className="link-btn" href="https://github.com/enriquezm">github</a>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="flex-content">
+            <p className="font-override">v1.1</p>
+          </div>
+        </div>
+      </header>
+
+      <main className="flex-container">
+        <section className="flex-column">
+          <PostContentContainer className="flex-content">
+            <h2>{title}</h2>
+            <div dangerouslySetInnerHTML={{__html: html}} />
+          </PostContentContainer>
+        </section>
+      </main>
+
+      {prev && 
+          <Link to={prev.frontmatter.path}>
+            Previous Post
+          </Link>
+        }
+      {next && 
+        <Link to={next.frontmatter.path}>
+          Next Post
+        </Link>
+      }
     </div>
   )
 }
 
-export const pageQuery = graphql`
+export const query = graphql`
   query($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
+    markdownRemark(frontmatter: {
+      path: {eq: $path} 
+    }) {
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        path
         title
       }
     }
   }
 `
+
+export default Template;
+  
