68f3fe9f86e6b8779a4508b4eee28f05568ea5ef
Myles Enriquez <enri.myles@gmail.com>
2019-02-05 11:02:11 -0800

    Update folder to read md from.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/projects`,
         name: "markdown-pages",
       },
     },
