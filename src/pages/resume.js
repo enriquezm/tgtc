5f1c32fbbfc0172f17eb24f6778b6db7444a5e58
Myles Enriquez <enri.myles@gmail.com>
2019-02-12 16:39:09 -0800

    Add formatting to content.

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,7 +4,21 @@ import styled from 'styled-components';
 
 const Resume = styled.div`
   section {
-    margin-bottom: 20px;
+    margin-bottom: 30px;
+    h2 {
+      margin-bottom: 8px;
+    }
+    ul {
+      margin: 5px 0;
+      li {
+        &:before {
+          content: '- ';
+        }
+      }
+    }
+    .block {
+      margin: 20px 0;
+    }
   }
 `
 
@@ -14,33 +28,33 @@ const ResumePage = () => {
       <Header />
       <section>
         <h2>Education</h2>
-        <div className="school-block">
-          <p>Information & Computer Science, B.A.<br />
-          University of Hawaii at Manoa<br />
+        <div className="block">
+          <h3>Information & Computer Science, B.A.</h3>
+          <p>University of Hawaii at Manoa<br />
           2010-2015</p>
         </div>
       </section>
       <section>
         <h2>Experience</h2>
-        <div className="job-block">
+        <div className="block">
           <h3>Web Specialist January 2017 - Present</h3>
           <p>UNLV, University Marketing & Communications</p>
           <ul>
-            <li>Converted spreadsheet GPA Calculator into an interactive web application, using JavaScript and Drupal (https://www.unlv.edu/academics/gpacalculator).</li>
+            <li>Converted spreadsheet GPA Calculator into an <a href="https://www.unlv.edu/academics/gpacalculator">interactive web application</a>, using JavaScript and Drupal.</li>
             <li>Redesigned and refactored degrees directory frontend user interface.</li>
             <li>Provided site analytics reports for department coordinators and team.</li>
             <li>Wrote documentation for degrees directory and site analytics using Gitbook.</li>
           </ul>
         </div>
-        <div className="job-block">
+        <div className="block">
           <h3>Web Developer May 2011 - May 2013</h3>
           <p>Distance Course Design & Consulting</p>
           <ul>
-            <li>Designed and developed the Assist tutorial site using a static site generator (Jekyll).</li>
+            <li>Designed and developed the <a href="https://assist.coe.hawaii.edu/">Assist tutorial site</a> using a static site generator (Jekyll).</li>
             <li>Assisted developers with website enhancements within Wordpress sites.</li>
           </ul>
         </div>
-        <div className="job-block">
+        <div className="block">
           <h3>Web Design Assistant August 2010 - May 2011</h3>
           <p>Distance Course Design & Consulting</p>
           <ul>
@@ -52,13 +66,13 @@ const ResumePage = () => {
       </section>
       <section>
         <h2>Projects</h2>
-        <div className="project-block">
+        <div className="block">
           <h3>Habit Rabbit</h3>
           <p>A habit forming application. Aimed to start, manage, and maintain a habit.</p>
           <p>Utilizes: Reactjs, Nodejs, mysql</p>
           <p>Source: https://github.com/enriquezm/habbit-hub</p>
         </div>
-        <div className="project-block">
+        <div className="block">
           <h3>Scoreboard Application</h3>
           <p>React application connecting to a firebase database.</p>
           <p>Utilizes: Reactjs, Firebase</p>
@@ -67,8 +81,10 @@ const ResumePage = () => {
       </section>
       <section>
         <h2>Skills</h2>
-        <p>(proficient): HTML, CSS, SCSS, JavaScript, Git, CLI, wireframing/prototyping</p>
-        <p>(familiar): A11y, Nodejs, Gatsby, React, MySQL</p>
+        <div className="block">
+          <p>(proficient): HTML, CSS, SCSS, JavaScript, Git, CLI, wireframing/prototyping</p>
+          <p>(familiar): A11y, Nodejs, Gatsby, React, MySQL</p>
+        </div>
       </section>
     </Resume>
   )
