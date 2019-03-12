4884eda471ad28f57280c863bd3f15511ff5f5e0
Myles Enriquez <enri.myles@gmail.com>
2019-03-12 15:32:38 -0700

    Add profile image.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,14 @@ import { Link } from 'gatsby'
 const Header = (props) => {
   return (
     <header className="flex-container flex-split margin-bottom-lg">
-        <div>
+        <div class="flex-container">
+          <div className="image-container"></div>
           <div className="flex-content">
             <Link to="/"><h1>Myles Enriquez</h1></Link>
             <p className="font-color-violet">Frontend Engineer</p>
             <div>
               {
-                props.extraBtn ? <Link to="/" className="link-btn margin-right-sm">More Projects</Link> : ''
+                props.extraBtn ? <Link to="/" className="link-btn margin-right-sm">All Projects</Link> : ''
               }
               <Link to="/resume/" className="link-btn">Resume</Link>
               <a className="link-btn" href="https://github.com/enriquezm">Github</a>
@@ -19,7 +20,7 @@ const Header = (props) => {
         </div>
         <div>
           <div className="flex-content">
-            <p className="font-override" title="Currently running version 1.1 of my site.">v1.1</p>
+            <p className="font-override" title="Hey you! Welcome to my site! :)">???</p>
           </div>
         </div>
       </header>
