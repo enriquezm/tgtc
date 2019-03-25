5c1815a779963745e29a376f5cbf0947a5997f1b
Myles Enriquez <enri.myles@gmail.com>
2019-03-25 14:59:46 -0700

    Import Header styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import './Header.css'
 
 const Header = (props) => {
   return (
     <header className="flex-container flex-split margin-bottom-lg">
-        <div class="flex-container">
-          <div className="image-container"></div>
-          <div className="flex-content">
-            <Link to="/"><h1>Myles Enriquez</h1></Link>
-            <p className="font-color-violet">Frontend Engineer</p>
-            <div>
-              {
-                props.extraBtn ? <Link to="/" className="link-btn margin-right-sm">All Posts</Link> : ''
-              }
-              <Link to="/resume/" className="link-btn">Resume</Link>
-              <a className="link-btn" href="https://github.com/enriquezm">Github</a>
-            </div>
+      <div class="flex-container">
+        <div className="image-container"></div>
+        <div className="flex-content">
+          <Link to="/"><h1>Myles Enriquez</h1></Link>
+          <p className="font-color-violet">Frontend Engineer</p>
+          <div>
+            {
+              props.extraBtn ? <Link to="/" className="link-btn margin-right-sm">All Posts</Link> : ''
+            }
+            <Link to="/resume/" className="link-btn">Resume</Link>
+            <a className="link-btn" href="https://github.com/enriquezm">Github</a>
           </div>
         </div>
-        <div>
-          <div className="flex-content">
-            <p className="font-override" title="Hey you! Welcome to my site! :)">;)</p>
-          </div>
+      </div>
+      <div>
+        <div className="flex-content">
+          <p className="font-override" title="Hey you! Welcome to my site! :)">;)</p>
         </div>
-      </header>
+      </div>
+    </header>
   )
 }
 
