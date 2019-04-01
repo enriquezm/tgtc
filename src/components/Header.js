f7e7069490dd7723b069cfd5c76c5f5ead049fdb
Myles Enriquez <enri.myles@gmail.com>
2019-04-01 14:51:10 -0700

    Simplify logic.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,26 +4,18 @@ import './Header.css'
 
 const Header = (props) => {
   return (
-    <header className="flex-container flex-split margin-bottom-lg">
-      <div class="flex-container">
-        <div className="image-container"></div>
-        <div className="flex-content">
-          <Link to="/"><h1>Myles Enriquez</h1></Link>
-          <p className="font-color-violet">Frontend Engineer</p>
-          <div>
-            {
-              props.extraBtn ? <Link to="/" className="link-btn margin-right-sm">All Posts</Link> : ''
-            }
-            <Link to="/resume/" className="link-btn">Resume</Link>
-            <a className="link-btn" href="https://github.com/enriquezm">Github</a>
-          </div>
+    <header>
+      <nav>
+        <Link to="/">
+          <h5>TGTC</h5>
+        </Link>
+        <div>
+          <Link to="/resume/">Resume</Link>
         </div>
-      </div>
-      <div>
-        <div className="flex-content">
-          <p className="font-override" title="Hey you! Welcome to my site! :)">;)</p>
-        </div>
-      </div>
+      </nav>
+      {
+        props.title ? <section className="padding-xlg centered"><h1>{props.title}</h1><p>{props.description}</p></section> : <section className="padding-xlg centered"><h1>Hello!</h1><p>I'm a Frontend Engineer creating simple interfaces connected to data.</p></section>
+      }
     </header>
   )
 }
