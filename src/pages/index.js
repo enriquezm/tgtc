273f174ce6293b01c8f98ed9cf2668d117ec9d16
Myles Enriquez <enri.myles@gmail.com>
2019-05-31 16:31:54 -0700

    Import graphql.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Contact from "../components/Contact";
