9200b5e6b0262ddffae10fbc182984d53c9ac893
Myles Enriquez <enri.myles@gmail.com>
2019-06-06 08:05:00 -0700

    Remove require statements, no longer needed.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,1 @@
-require("prismjs/themes/prism-okaidia.css");
-require("prismjs/plugins/line-numbers/prism-line-numbers.css");
-
-// require("themes/prism-duotone-dark.css");
+// File is empty for now.
