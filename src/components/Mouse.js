8e38e74321754f489a7922e7f536f752c271f5ad
Myles Enriquez <enri.myles@gmail.com>
2019-06-06 12:55:27 -0700

    Add color prop.

--- src/components/Mouse.js
+++ src/components/Mouse.js
@@ -32,14 +32,14 @@ const SVG = styled.svg`
   }
 `;
 
-const Mouse = () => (
+const Mouse = props => (
   <SVG width="40px" height="72px" viewBox="0 0 19 51" version="1.1" xmlns="http://www.w3.org/2000/svg">
     <title>Mouse</title>
     <desc>Created with Sketch.</desc>
     <g id="Mouse" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g>
-            <rect id="Rectangle" stroke="#000000" x="0.5" y="0.5" width="18" height="33" rx="8"></rect>
-            <circle id="Scroll-Wheel" fill="#000000" cx="9.5" cy="7.5" r="2.5"></circle>
+            <rect id="Rectangle" stroke={props.color ? props.color : "black"} x="0.5" y="0.5" width="18" height="33" rx="8"></rect>
+            <circle id="Scroll-Wheel" fill={props.color ? props.color : "black"} cx="9.5" cy="7.5" r="2.5"></circle>
         </g>
     </g>
   </SVG>
