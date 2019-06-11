c6c2d8faa21097db5e9890d3d3445f4f21b6005e
Myles Enriquez <enri.myles@gmail.com>
2019-06-11 13:20:34 -0700

    Create new textbutton component.

---
+++ src/components/util/TextButton.js
@@ -0,0 +1,19 @@
+import { Link } from "gatsby";
+import styled from "styled-components";
+
+const TextButton = styled(Link)`
+  margin-top: 25px;
+  border: 1px solid #009fff;
+  padding: 1% 2%;
+  &:hover {
+    color: white;
+    background: #009fff;
+  }
+  @media screen and (max-width: 600px) {
+    padding: 2% 4%;
+    width: 100%;
+    text-align: center;
+  }
+`;
+
+export default TextButton;
