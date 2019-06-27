489ff8273da3bfb528237a44f02d3cd2968bdf3d
Myles Enriquez <enri.myles@gmail.com>
2019-06-27 15:14:39 -0700

    Add code items.

--- src/pages/about.js
+++ src/pages/about.js
@@ -52,7 +52,7 @@ const IndexPage = ({ data }) => {
   return (
     <div>
       <Helmet>
-        <title>That Guy That Codes | Recruiters</title>
+        <title>Myles Enriquez | ThatGuyThatCodes</title>
         <link rel="canonical" href="https://thatguythat.codes" />
       </Helmet>
       <Header
@@ -60,6 +60,26 @@ const IndexPage = ({ data }) => {
         description="A glipse of who I am, what I've built, and where I want to go in my career."
         full
       />
+      <Section>
+        <h2>My Coder's Code</h2>
+        <p>
+          The following items are adopted from the{' '}
+          <a href="https://www.amazon.com/Pragmatic-Programmer-Journeyman-Master/dp/020161622X">
+            Pragmatic Programmer
+          </a>
+          . I try to stick to these as my own personal programmer's code.
+        </p>
+        <ul>
+          <li>Provide options, don't make lame excuses.</li>
+          <li>DRY - Don't Repeat Yourself.</li>
+          <li>Use the power of command shells.</li>
+          <li>Always use source code control.</li>
+          <li>Invest regularly in your knowledge portfolio.</li>
+          <li>Prototype to learn.</li>
+          <li>Fix the problem, not the blame.</li>
+          <li>Design to test.</li>
+        </ul>
+      </Section>
       <Section>
         <h2>Activity</h2>
         <p>My latest activity on Github.</p>
@@ -87,7 +107,6 @@ const IndexPage = ({ data }) => {
           })}
         </Grid>
       </Section>
-      <Contact />
       <Footer />
     </div>
   );
