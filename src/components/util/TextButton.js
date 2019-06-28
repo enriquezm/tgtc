7035b768119c2e352c9f1f5322c100f36c716f6f
Myles Enriquez <enri.myles@gmail.com>
2019-06-28 16:22:22 -0700

    Set primary and secondary styles.

--- src/components/util/TextButton.js
+++ src/components/util/TextButton.js
@@ -1,13 +1,39 @@
 import { Link } from 'gatsby';
 import styled from 'styled-components';
 
+const primaryColor = {
+  normal: '#7469F4',
+  hover: '#685edb',
+  click: '#5c54c3',
+};
+
+const secondaryColor = {
+  normal: '#7469F4',
+  hover: '#9d96f7',
+  click: '#b9b4f9',
+};
+
 const TextButton = styled(Link)`
-  margin-top: 25px;
-  border: 1px solid #009fff;
+  margin: 0 5px;
+  width: 100%;
+  border-radius: 4px;
+  text-align: center;
+  background-color: ${props => (props.primary ? primaryColor.normal : 'none')};
+  border: 1px solid
+    ${props => (props.primary ? primaryColor.normal : secondaryColor.normal)};
+  color: ${props => (props.primary ? 'white' : secondaryColor.normal)};
   padding: 1% 2%;
   &:hover {
-    color: white;
-    background: #009fff;
+    color: ${props => (props.primary ? 'white' : secondaryColor.hover)};
+    border: 1px solid
+      ${props => (props.primary ? primaryColor.hover : secondaryColor.hover)};
+    background-color: ${props => (props.primary ? primaryColor.hover : 'none')};
+  }
+  &:active {
+    color: ${props => (props.primary ? 'white' : secondaryColor.click)};
+    border: 1px solid
+      ${props => (props.primary ? primaryColor.click : secondaryColor.click)};
+    background-color: ${props => (props.primary ? primaryColor.click : 'none')};
   }
   @media screen and (max-width: 600px) {
     padding: 2% 4%;
